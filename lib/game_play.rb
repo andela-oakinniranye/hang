b1f17186c1e8c8eb4e9a9f6aff25a1d09a2ca9e2
andela.atemitope <amodut@gmail.com>
2015-10-06 12:55:11 +0100

    added comments to game_play file

--- lib/game_play.rb
+++ lib/game_play.rb
@@ -5,6 +5,7 @@ class GamePlay
   attr_reader :word, :remaining_letters,:guess
 
   def get_user_input
+# This method gets the user's input and calls the respective methods to handle the input
     puts Display.new.intro
     sleep 3
     puts Display.new.main_menu
@@ -27,6 +28,7 @@ class GamePlay
 
 
   def load_libraries
+    # This method loads the library of words, filters the characters length and selects the word to be guessed
     @dictionary_array = File.readlines "5desk.txt"
     @dictionary_array.delete_if {|x| x.length < 6  || x.length >  14}  
     @new_dictionary = @dictionary_array.shuffle!
@@ -40,6 +42,7 @@ class GamePlay
 
 
   def lives
+    # this method instanciates the user's available guesses based on the length of the word to be guessed
     if (@word.length) > 7
       @lives = (@word.length / 2) + 3
     else 
@@ -49,11 +52,13 @@ class GamePlay
   end
 
   def display_lives
+    # This method displays the amount of guesses left for the user
     puts "\n You have #{@lives} guess(es) left"
     visual_update
   end
 
   def initial_visual_update
+    # this method shows the default number of missing letters in the word to be guesssed
     @answer = Array.new(@word.length)
     @answer.map! do |item|
       item = "_" 
@@ -65,6 +70,7 @@ class GamePlay
 
 
   def visual_update
+    # this method updates the user's input after each correct guess
     @visual = @answer
     if @word.include? @guess
       @word.each_with_index do |letter, index|
@@ -79,6 +85,11 @@ class GamePlay
 
 
   def game_play
+=begin
+this method checks for the user's remaining guesses and success status. 
+    It calls the appropriate method required to handle each condition
+=end
+
     if @lives == 0
       game_over
     elsif @lives > 0 && @remaining_letters.length == 0
@@ -92,6 +103,10 @@ class GamePlay
   end 
 
   def save_quit
+=begin
+    this method handles the user menu option to either save or load a particular game and calls the respective method based on user's input
+    it also provides available options for exit 
+=end
       puts Display.new.display_save_menu
       @save_quit = gets.strip
       case @save_quit
@@ -106,6 +121,10 @@ class GamePlay
   end
 
   def input_check
+=begin
+    this method filters the users input and ensures the user inputs a letter 
+    It also calls the method to handle each respective exception
+=end
     if @remaining_letters.include? (@guess)
       good_guess
     elsif /^[A-Za-z, *]$/ !~ @guess
@@ -120,6 +139,7 @@ class GamePlay
 
 
   def good_guess
+    # this method is called when the user makes correct guess
     @remaining_letters.delete(@guess)
     puts Display.new.display_good_guess
     display_lives 
@@ -128,6 +148,7 @@ class GamePlay
 
 
   def wrong_guess
+    # this method is called when a user makes a wrong guess
     puts Display.new.display_wrong_guess
     @lives -= 1
     display_lives 
@@ -136,6 +157,7 @@ class GamePlay
 
 
   def game_over
+    # this method is called when the user has no available guesses left
     puts "\n THE CORRECT WORD IS: #{@word.join(' ')}"
     puts Display.new.display_game_over
     restart_gameover = gets.chomp.downcase.strip
@@ -150,6 +172,7 @@ class GamePlay
 
 
   def overall_success
+    # this method is called when the user has guessed the word completely
     puts Display.new.display_good_game
     restart_success = gets.chomp.downcase.strip
     case restart_success
@@ -163,6 +186,7 @@ class GamePlay
 
 
  def save_game
+  # this method handles the option for saving the user's game 
   puts "\nInput a custom name for your saved data\n"
   @username = gets.chomp.strip
     if @username.length > 8 
@@ -177,6 +201,7 @@ class GamePlay
 
 
   def save_data 
+    # this method is called by the save_game method and handles the responsibilty of writing the game's state to a txt file
     File.open(@save_name,'w+') do | line |
       line.puts @word.join(",")
       line.puts @remaining_letters.join(",")
@@ -188,6 +213,7 @@ class GamePlay
 
 
   def load_game
+    # this method handles the loading of your file 
     puts "Type the name of your saved game"
     @load_username = STDIN.gets.chomp.downcase
     if File.file?("#{@load_username}"+".txt")
@@ -200,6 +226,7 @@ class GamePlay
   end
 
   def load_file
+    # this method handles the reading of the reqired file to be loaded
     @load_file = File.readlines("#{@load_username}"+".txt")
     @load_file.map! do |item|  
       item.chomp.delete(",")
