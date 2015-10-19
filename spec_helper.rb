1ee47018f8d8bf1974af9d8f3456c439386f6e22
andela.atemitope <amodut@gmail.com>
2015-10-19 13:12:20 +0100

    splited the hangman_spec.rb files

---
+++ spec_helper.rb
@@ -0,0 +1,14 @@
+# //require "coveralls"
+# //Coveralls.wear!
+require "simplecov"
+SimpleCov.start
+require "simplecov-shield"
+require "fakefs/safe"
+require_relative "../lib/hangman/save_load.rb"
+require_relative "../lib/hangman/game_display.rb"
+require_relative "../lib/hangman/dictionary.rb"
+require_relative "../lib/hangman.rb"
+require_relative "../lib/hangman/win_lose.rb"
+
+
+SimpleCov.formatter = SimpleCov::Formatter::ShieldFormatter
